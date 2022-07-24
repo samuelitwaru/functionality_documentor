f47dfb013b82657a3b879f7a869f1b5f140b07f8
samuelitwaru <samuelitwaru@gmail.com>
2022-07-24 06:15:16 +0300

    path

--- functionality_documentor/settings.py
+++ functionality_documentor/settings.py
@@ -17,8 +17,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print(type(Path('/home')))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -88,6 +86,8 @@ DATABASES = {
         'NAME': Path(os.path.dirname(BASE_DIR)) / 'db.sqlite3',
     }
 }
+
+print('>>>>>', DATABASES['default']['NAME'])
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators

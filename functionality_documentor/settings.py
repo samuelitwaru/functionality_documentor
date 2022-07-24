3688f3512ed6309088525b71ce8079e2a34b441b
samuelitwaru <samuelitwaru@gmail.com>
2022-07-24 05:37:11 +0300

    move db out

--- functionality_documentor/settings.py
+++ functionality_documentor/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'functionality_documentor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.dirname(BASE_DIR) + 'db.sqlite3',
     }
 }
 
